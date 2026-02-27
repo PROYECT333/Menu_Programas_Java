a157875222bb1242878dedd25862bd18869a7a35
THE_CHARLY <THE_CHARLY@192.168.1.77>
2026-02-27 01:38:47 -0600

    Iniciando commit

---
+++ src/menu_programas_y_metodos/Menu_Programas_y_Metodos.java
@@ -0,0 +1,17 @@
+package menu_programas_y_metodos;
+import java.util.Scanner;
+/**
+ *
+ * @author THE_CHARLY
+ */
+public class Menu_Programas_y_Metodos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner tecla = new Scanner(System.in);
+        System.out.println("Este programa usa metodos");
+    }
+    
+}
