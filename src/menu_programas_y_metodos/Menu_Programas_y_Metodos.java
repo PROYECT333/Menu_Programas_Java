626f5f9e584c4e35170282780d672852c59d547a
JUAN CARLOS <THE_CHARLY@192.168.1.77>
2026-02-27 01:48:13 -0600

    Creacion de variables

--- src/menu_programas_y_metodos/Menu_Programas_y_Metodos.java
+++ src/menu_programas_y_metodos/Menu_Programas_y_Metodos.java
@@ -10,8 +10,18 @@ public class Menu_Programas_y_Metodos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Variables de los programas
         Scanner tecla = new Scanner(System.in);
-        System.out.println("Este programa usa metodos");
+        int op, opc,edad, num, p1, p2 ,p3,n,n2,n3, nt,r, nm2, nm3, sm1;
+        int s=0, s1=0;
+        int secreto = 5;
+        int nump;
+        while(true){
+        System.out.println("Este programa muestra un menu de programas");
+        System.out.println("Presiona: 1-Metodos(sumar, restar, multiplicar, dividir), 2-Verificar si eres mayor de edad\n"
+                + "3-Indicar si el numero es positivo o negativo, 4-indicar que numero es mayor, 5-Adivina el numero, 6-Salir");
+        op = tecla.nextInt();
+        }
     }
     
 }
